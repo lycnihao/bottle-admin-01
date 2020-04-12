9ec691ffeed1abbb5497c8865c93c648c5d42b52
lycnihao <38707145@qq.com>
2020-04-12 21:55:50 +0800

    m

--- src/layouts/UserLayout.vue
+++ src/layouts/UserLayout.vue
@@ -5,11 +5,11 @@
         <div class="header">
           <a href="/">
             <img src="~@/assets/logo.svg" class="logo" alt="logo">
-            <span class="title">Ant Design</span>
+            <span class="title">Bottle Admin</span>
           </a>
         </div>
         <div class="desc">
-          Ant Design 是西湖区最具影响力的 Web 设计规范
+          Bottle 是一款简单的存储系统
         </div>
       </div>
 
@@ -22,7 +22,7 @@
           <a href="_self">条款</a>
         </div>
         <div class="copyright">
-          Copyright &copy; 2018 白鹭学园技术组出品
+          Copyright &copy; 2020 Liycheng
         </div>
       </div>
     </div>
